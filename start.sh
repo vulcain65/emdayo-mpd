9c6ad43b01433c5e3f666a3cb5fe8e7161c83252
daniel <daniel.doussaud@gmail.com>
2020-01-11 18:13:48 +0100

    ADD copy mpd.conf & chown dir

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-echo $0
+echo "--- $0"
 
 if [ -n "${GID_AUDIO}" ]; then
     addgroup -g ${GID_AUDIO} audio2
     adduser mpd audio2
 fi
+
+if [ ! -f "/mpd/conf/mpd.conf" ]; then
+    cp addgroup -g ${GID_AUDIO} audio2
+    adduser mpd audio2
+fi
+
+
+chown -R mpd:audio /mpd && chown -R mpd:audio /run/mpd/
+
 /usr/bin/mpd --no-daemon --stdout /mpd/conf/mpd.conf
